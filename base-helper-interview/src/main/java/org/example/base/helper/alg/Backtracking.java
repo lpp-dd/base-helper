352f3f97b3a9897e9ba6193ddfb0920b37a8773f
panfudong002 <panfudong002@ke.com>
2024-01-05 17:47:54 +0800

    回溯算法解决皇后和括号问题

--- base-helper-interview/src/main/java/org/example/base/helper/alg/Backtracking.java
+++ base-helper-interview/src/main/java/org/example/base/helper/alg/Backtracking.java
@@ -1,11 +1,11 @@
 package org.example.base.helper.alg;
 
-import com.alibaba.fastjson.JSON;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author panfudong
@@ -25,7 +25,24 @@ public class Backtracking {
      * 输出：["255.255.11.135", "255.255.111.35"]
      */
     public static void main(String[] args) {
-        System.out.println(JSON.toJSONString(restoreIp("25525511135")));
+        List<String> left = new ArrayList<>();
+        left.add("(");
+        left.add("(");
+        left.add("(");
+
+        List<String> right = new ArrayList<>();
+        right.add(")");
+        right.add(")");
+        right.add(")");
+
+        List<String> result = new ArrayList<>();
+        bt(3, left, right, 0, 0, new ArrayList<>(), result);
+
+        System.out.println(result);
+
+        List<String> queenResult = new ArrayList<>();
+        btQueen(5, 0, new ArrayList<>(), queenResult);
+        System.out.println(queenResult);
     }
 
     public static List<String> restoreIp(String ipAddress) {
@@ -56,4 +73,67 @@ public class Backtracking {
             }
         }
     }
+
+    /**
+     * 当给定 n 对括号时，任务是生成所有合法的括号组合。合法的括号组合是这样的，每个左括号 "(" 必须有相应的右括号 ")"，且不得存在嵌套的情况。例如，对于 n = 3，合法的括号组合可以是：
+     * [  "((()))",  "(()())",  "(())()",  "()(())",  "()()()"]
+     * @param n
+     * @param left
+     * @param right
+     * @param leftIndex
+     * @param rightIndex
+     * @param resultItem
+     * @param result
+     */
+    public static void bt(int n, List<String> left, List<String> right, int leftIndex, int rightIndex, List<String> resultItem, List<String> result) {
+        if (resultItem.size() == 2*n && leftIndex == n && rightIndex == n) {
+            result.add(String.join( "", resultItem));
+            return;
+        }
+        if (leftIndex < n) {
+            resultItem.add(left.get(leftIndex));
+            bt(n, left, right, leftIndex + 1, rightIndex, resultItem, result);
+            resultItem.remove(resultItem.size() - 1);
+        }
+        if (rightIndex < leftIndex) {
+            resultItem.add(right.get(rightIndex));
+            bt(n, left, right, leftIndex, rightIndex + 1, resultItem, result);
+            resultItem.remove(resultItem.size() - 1);
+        }
+    }
+
+    /**
+     * 在一个 N x N 的棋盘上放置 N 个皇后，使得它们不能互相攻击，即任意两个皇后不能在同一行、同一列或同一对角线上。
+     * 给定一个整数 N，返回所有不同的 N 皇后问题的解决方案。
+     * @param n
+     * @param i
+     * @param queenList
+     * @param result
+     */
+    public static void btQueen(int n, int i, List<Integer> queenList, List<String> result) {
+        if (queenList.size() == n) {
+            result.add(queenList.stream().map(String::valueOf).collect(Collectors.joining("-")));
+            return;
+        }
+        while (i < n) {
+            if (queenList.size() == 0 || canAdd(queenList, i)) {
+                queenList.add(i);
+                btQueen(n, 0, queenList, result);
+                queenList.remove(queenList.size() - 1);
+            }
+            i++;
+        }
+    }
+
+    private static boolean canAdd(List<Integer> queenList, int currentI) {
+        for (int i = 0; i < queenList.size(); i++) {
+            int rowInterval = Math.abs(queenList.size() - i);
+            int colInterval = Math.abs(queenList.get(i) - currentI);
+            if (rowInterval == colInterval || colInterval == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
