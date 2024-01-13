c74e4f6c40a65a7e5edb85dec979bcddff6deae7
panfudong002 <panfudong002@ke.com>
2024-01-13 16:24:06 +0800

    快排

--- base-helper-interview/src/main/java/org/example/base/helper/alg/SortTest.java
+++ base-helper-interview/src/main/java/org/example/base/helper/alg/SortTest.java
@@ -1,5 +1,6 @@
 package org.example.base.helper.alg;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -10,6 +11,19 @@ import java.util.Objects;
 public class SortTest {
 
     public static void main(String[] args) {
+        List<Integer> sourceList = new ArrayList<>();
+
+        sourceList.add(3);
+        sourceList.add(1);
+        sourceList.add(6);
+        sourceList.add(9);
+        sourceList.add(2);
+        sourceList.add(4);
+        sourceList.add(8);
+        quickSorted(sourceList);
+        System.out.println(sourceList);
+
+
     }
 
     /**
@@ -41,6 +55,40 @@ public class SortTest {
     private static List<Integer> merge(List<Integer> s1, List<Integer> s2) {
         //合并两个有序链表
         return null;
+    }
+
+
+    public static void quickSorted(List<Integer> list) {
+        if (list == null || list.size() <= 1) {
+            return;
+        }
+        quickSorted(list, 0, list.size() - 1);
+    }
+
+    public static void quickSorted(List<Integer> list, int l, int r) {
+        int base = list.get(l);
+        int left = l + 1;
+        int right = r;
+        while (left <= right) {
+            while (list.get(left) < base && left <= right) {
+                left++;
+            }
+            while (list.get(right) > base && left <= right) {
+                right--;
+            }
+            if (left < right) {
+                swap(list, left, right);
+            }
+        }
+        swap(list, l, left);
+        quickSorted(list, l, left - 1);
+        quickSorted(list, left + 1, r);
+    }
+
+    private static void swap(List<Integer> list, int a, int b) {
+        int temp = list.get(a);
+        list.set(a, list.get(b));
+        list.set(b, temp);
     }
 
 }
