aba743e3f2c450320e8991d12ec797445fe5f899
panfudong002 <panfudong002@ke.com>
2024-01-29 16:32:31 +0800

    反转链表2

--- base-helper-interview/src/main/java/org/example/base/helper/alg/LinkedListTest.java
+++ base-helper-interview/src/main/java/org/example/base/helper/alg/LinkedListTest.java
@@ -1,5 +1,7 @@
 package org.example.base.helper.alg;
 
+import java.util.List;
+
 /**
  * @author panfudong
  * @description
@@ -7,20 +9,13 @@ package org.example.base.helper.alg;
 public class LinkedListTest {
 
     public static void main(String[] args) {
-        Node head = new Node();
-        head.value = 1;
-        Node node1 = new Node();
-        node1.value = 2;
-        Node node2 = new Node();
-        node2.value = 3;
-        Node node3 = new Node();
-        node3.value = 4;
-
+        ListNode head = new ListNode();
+        head.val = 3;
+        ListNode node1 = new ListNode();
+        node1.val = 5;
         head.next = node1;
-        node1.next = node2;
-        node2.next = node3;
         //reverse(head);
-        Node newHead = rec(head);
+        ListNode newHead = reverseBetween(head, 1, 2);
         System.out.println(newHead);
     }
 
@@ -29,17 +24,17 @@ public class LinkedListTest {
      * @param head
      * @return
      */
-    public static Node reverse(Node head) {
+    public static ListNode reverse(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
         //声明一个变量代表你的新儿子
-        Node prev = null;
+        ListNode prev = null;
         // A -> B -> C
-        Node current = head;
+        ListNode current = head;
         while (current != null) {
             //B -> C  首先获取你目前的儿子
-            Node next = current.next;
+            ListNode next = current.next;
             //A -> null  将你的新儿子给你
             current.next = prev;
             //将 A->null作为 prev节点  你和你的新儿子将打包作为你旧儿子的新儿子
@@ -50,12 +45,12 @@ public class LinkedListTest {
         return prev;
     }
 
-    public static Node rec(Node node) {
+    public static ListNode rec(ListNode node) {
         if (node == null || node.next == null) {
             return node;
         }
         //递归找儿子
-        Node newHead = rec(node.next);
+        ListNode newHead = rec(node.next);
         //儿子要当爹
         node.next.next = node;
         node.next = null;
@@ -63,8 +58,54 @@ public class LinkedListTest {
     }
 
 
-    public static class Node {
-        private Node next;
-        private int value;
+    public static class ListNode {
+        private ListNode next;
+        private int val;
+    }
+
+    /**
+     * https://leetcode.cn/problems/reverse-linked-list-ii/description/
+     * 反转部分链表
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || head.next == null || left == right) {
+            return head;
+        }
+        ListNode start = null;
+        ListNode end = null;
+        ListNode cur = head;
+        ListNode prev = null;
+        ListNode sourceLeft = null;
+        ListNode sourceRight = null;
+        for (int i = 1; i <= right; i++) {
+            if (i == left) {
+                sourceLeft = cur;
+            }
+            if (i == right) {
+                end = cur.next;
+                sourceRight = cur;
+            }
+            if (i >= left) {
+                ListNode next = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = next;
+            } else {
+                start = cur;
+                cur = cur.next;
+            }
+        }
+        if (sourceLeft != null) {
+            sourceLeft.next = end;
+        }
+        if (start != null) {
+            start.next = sourceRight;
+            return head;
+        }
+        return prev;
     }
 }
